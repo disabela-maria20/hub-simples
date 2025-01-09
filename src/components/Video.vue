5c3d1a368b9e4b2b860cb0763f19cd77493c71c5
Isabela M. <isabela10014@hotmail.com>
2025-01-09 16:14:14 -0300

    feat: add novo filme

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -3,7 +3,7 @@
     <img src="~@/assets/bg-trailer.jpg" @click="clicked = true">
   </div>
   <div v-else class="video embed-responsive embed-responsive-16by9">
-    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/iCMLLgEJ1QE?si=SDcIg4Sm32PRmB8z"
+    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Ac5fp_7Be9I?si=i7xFzE1La_WvW-y_"
       title="YouTube video player" frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen></iframe>
