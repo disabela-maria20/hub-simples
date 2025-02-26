f8d16dab21b5c939f6ccc67367734a7e2b985800
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-02-26 12:23:21 -0300

    feat: novo hub

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -3,7 +3,7 @@
     <img src="~@/assets/bg-trailer.jpg" @click="clicked = true">
   </div>
   <div v-else class="video embed-responsive embed-responsive-16by9">
-    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/iCMLLgEJ1QE?si=SDcIg4Sm32PRmB8z"
+    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/pvJvFPsAkdw?si=j1T-SWSldMqDk05Z"
       title="YouTube video player" frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen></iframe>
