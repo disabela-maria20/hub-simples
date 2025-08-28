885223be0876e47a197f6bb0b744212a0c6db22e
Isabela M Ribeiro <isabela10014@hotmail.com>
2025-08-28 19:10:43 -0300

    feat: add hub

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -3,7 +3,7 @@
     <img src="~@/assets/bg-trailer.jpg" @click="clicked = true">
   </div>
   <div v-else class="video embed-responsive embed-responsive-16by9">
-    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/hsc8SBOF-9A?si=kG7uUBAWX6geOGQR"
+    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/yFn9j64_5V0?si=kVDyWHO8-nM5HH2B"
       title=" YouTube video player" frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen></iframe>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: '/umloboentreoscisnes/',
+    publicPath: '/dormirdeolhosabertos/',
 };
