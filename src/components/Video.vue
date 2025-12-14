90656e998a53d9bd17d6fa90ad72d55447fe64eb
Isabela Maria Ribeiro Da Silva <imsilva9@topazevolution.com>
2025-12-14 13:36:51 -0300

    add hub

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -3,7 +3,7 @@
     <img src="~@/assets/bg-trailer.jpg" @click="clicked = true">
   </div>
   <div v-else class="video embed-responsive embed-responsive-16by9">
-    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/u1CEoXBNXAM?si=uk94FoTlcjBdarwN"
+    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/M9TNwQR36mA?si=6ydIUB10ZPvvGBgE"
       title=" YouTube video player" frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen></iframe>
