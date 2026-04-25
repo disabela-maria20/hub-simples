c327230cb36b12e1c8082c2dcfaf1e5fb01cf2ca
Isabela Maria Ribeiro Da Silva <imsilva9@topazevolution.com>
2026-04-25 10:03:05 -0300

    feat: ajuste no bb

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@
       <div class="footer_wrapper">
         <div class="container">
           <div class="links_footer">
-            <div class="cp"> © 2026 IMAGEM FILMES</div>
+            <div class="cp"> © 2026 VITRINE PETROBRAS</div>
             <img src="./assets/bb.png" alt="">
             <div class="btn-creditos" :class="{ credito_ativo: credito_ativo }" @click="carregar()">
               Acessibilidade
